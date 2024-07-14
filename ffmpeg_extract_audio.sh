d51c0b321835d4ae8de9cebbc7262d495834ce2d
Daniel Reynard <neptunemist@proton.me>
2024-07-14 07:51:20 +0000

    add: script

---
+++ ffmpeg_extract_audio.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+process_file() {
+    input_file="$1"
+    input_filename=$(basename "$input_file")
+    output_filename="${input_filename%.*}"
+
+    audio_codec=$(ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$input_file" | head -n 1)
+
+    # trimmed_audio_codec="${audio_codec##*( )}"
+    trimmed_audio_codec=$(echo "$audio_codec" | tr -cd '[:alnum:]')
+
+    if [ "$trimmed_audio_codec" == "aac" ] || [ "$trimmed_audio_codec" == "opus" ]; then
+        output_extension="$trimmed_audio_codec"
+
+        echo "Detected audio codec: $trimmed_audio_codec"
+    else
+        echo "Unsupported audio codec: $trimmed_audio_codec"
+        return
+    fi
+
+    output_path="$(dirname "$input_file")/$output_filename.${output_extension}"
+
+
+    ffmpeg -i "$input_file" -vn -acodec copy -y "$output_path" -loglevel quiet -stats
+
+    echo "Done."
+}
+
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 input_path"
+    exit 1
+fi
+
+# Get the provided input path
+input_path="$1"
+
+# List of most common video format
+video_extensions=("*.mp4" "*.mkv" "*.ts" "*.avi" "*.mov" "*.flv" "*.wmv" "*.m4v" "*.webm" "*.vob" "*.mpg" "*.mpeg" "*.3gp" "*.ogv")
+
+# Create an array of find options
+find_options=()
+for ext in "${video_extensions[@]}"; do
+    find_options+=(-o -iname "$ext")
+done
+
+# Remove the first element (-o) from the array
+find_options=("${find_options[@]:1}")
+
+# Check if the input path is only a directory
+if [ -d "$input_path" ]; then
+    find "$input_path" -type f \( "${find_options[@]}" \) -print0 | while IFS= read -r -d '' file; do
+        process_file "$file"
+	done
+else
+    process_file "$input_path"
+fi
